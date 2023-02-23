cd608734ca5ce73dd63587b817096a5446d79741
CrayzFrog-tech <allanmohamad7@gmail.com>
2023-02-23 18:29:50 +0100

    app bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule,  } from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list'
 
@@ -24,7 +24,8 @@ import {MatGridListModule} from '@angular/material/grid-list'
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
