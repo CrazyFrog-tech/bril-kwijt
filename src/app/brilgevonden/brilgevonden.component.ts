c28b523d4c723cdb0fafaa5f7329f7ed20617372
CrayzFrog-tech <allanmohamad7@gmail.com>
2023-03-22 08:54:45 +0100

    ik

---
+++ src/app/brilgevonden/brilgevonden.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brilgevonden',
+  templateUrl: './brilgevonden.component.html',
+  styleUrls: ['./brilgevonden.component.css']
+})
+export class BrilgevondenComponent {
+
+}
