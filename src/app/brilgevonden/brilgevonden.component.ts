21def82c6587d0ad252f4c6112e3bc92372ce69e
allana <mohamad.allan@rigd-loxia.nl>
2023-06-18 16:23:32 +0200

    date picker

--- src/app/brilgevonden/brilgevonden.component.ts
+++ src/app/brilgevonden/brilgevonden.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-brilgevonden',
@@ -8,6 +10,10 @@ import { Router } from '@angular/router';
 })
 export class BrilgevondenComponent {
   constructor(private router: Router) {}
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 
   goHome() {
     console.log('/homescreen');
