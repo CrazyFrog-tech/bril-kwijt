27e7fe5d89bd2484a6c4191dcc54cd38565a6159
allana <mohamad.allan@rigd-loxia.nl>
2023-08-08 17:54:27 +0200

    uploading almost there

--- src/app/components/brilgevonden/brilgevonden.component.ts
+++ src/app/components/brilgevonden/brilgevonden.component.ts
@@ -1,10 +1,13 @@
 import { Component, Directive } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormGroup, FormControl, FormsModule, ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../services/api.service';
 import { FakeBril } from '../../dao/fakebril';
 import { Address } from '../../dao/address';
+import PlaceResult = google.maps.places.PlaceResult;
+import { GermanAddress } from '@angular-material-extensions/google-maps-autocomplete';
+
 
 
 @Component({
@@ -13,11 +16,15 @@ import { Address } from '../../dao/address';
   styleUrls: ['./brilgevonden.component.css'],
 })
 export class BrilgevondenComponent {
+  public selectedAddress!: PlaceResult;
+
   selectedFiles: FileList | null = null;
   post: any = '';
   formGroup = new FormGroup({
     description: new FormControl(''),
   });
+
+  addressObject! : Address;
 
   lostAtFormGroup = new FormGroup({
     lostAt: new FormControl(new Date),
@@ -36,32 +43,43 @@ export class BrilgevondenComponent {
     private apiService: ApiService) {
 
   }
+
+  onAutoCompleteSelected(result: GermanAddress) {
+    let street = result.streetName || '';
+    let houseNr = result.streetNumber || '';
+    let zipCode = result.postalCode || '';
+    let city = result.state?.long || '';
+
+    this.addressObject = new Address(street, houseNr, "" + zipCode, city);
+
+    console.log('onAutocompleteSelected: ', result);
+  }
   getSelectedFiles(event: any): void {
     debugger;
     this.selectedFiles = event.target.files;
   }
 
   goHome() {
-    console.log(this.address.controls['address']);
+    //todo getting specifik data from the address.
+
     let description = '';
     description = this.formGroup.controls['description'].value as string;
     let dateLostAt = this.lostAtFormGroup.controls['lostAt'].value as Date;
     let formData = new FormData();
 
-    console.log('/homescreen');
-    console.log(this.formGroup.controls['description'].value);
     if (this.selectedFiles && this.selectedFiles.length) {
-    for (let i = 0; i < this.selectedFiles.length; i++) {
-      formData.append('images', this.selectedFiles[i]);
+      for (let i = 0; i < this.selectedFiles.length; i++) {
+        formData.append('images', this.selectedFiles[i]);
+      }
     }
-  }
-  let addrezz = this.address.controls['address'].value || " ";
-    let fakeBril = new FakeBril(description, dateLostAt, addrezz)
+    let addrezz = this.address.controls['address'].value || " ";
+    let fakeBril = new FakeBril(description, dateLostAt, this.addressObject)
     formData.append('bril', JSON.stringify(fakeBril));
-    console.log(formData);
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ', ' + pair[1]);
+    }
     this.apiService.addFakeBril(formData).subscribe(
-      data =>{
-        console.log(data);
+      data => {
       }
     )
     // DEBUG YHR POST METHOD WHY IT DOEN'T WORK
