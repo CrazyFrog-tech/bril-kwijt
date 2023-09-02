9289af3239cf6bc537160dd38ddded3d770f42a4
allana <mohamad.allan@rigd-loxia.nl>
2023-09-02 12:17:40 +0200

    gettin and showing images

--- src/app/components/bril-advertentie/bril-advertentie.component.ts
+++ src/app/components/bril-advertentie/bril-advertentie.component.ts
@@ -5,8 +5,10 @@ import { Store } from 'redux';
 import { FakeBril } from 'src/app/dao/fakebril';
 import { ApiService } from 'src/app/services/api.service';
 import { HttpParams } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 import { map } from 'rxjs';
-import { SafeUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bril-advertentie',
@@ -16,15 +18,14 @@ import { SafeUrl } from '@angular/platform-browser';
 export class BrilAdvertentieComponent implements OnInit {
   id: string = '';
   bril!: FakeBril;
-  sanitizer: any;
   images: SafeUrl[] = [];
 
 
-  constructor(private brilIdService: BrilService, private apiService: ApiService) {
+  constructor(private brilIdService: BrilService, private apiService: ApiService, private sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
-    debugger
+
     this.brilIdService.selectedId$.subscribe((value) => {
       this.id = value;
       console.log(this.id);
@@ -34,7 +35,7 @@ export class BrilAdvertentieComponent implements OnInit {
           for(let imagePath of this.bril.imageFilenames){
             this.apiService.getImages(new HttpParams().append('imageName', imagePath)).pipe(
               map((imageBlob) => {
-                const blob = new Blob([imageBlob], { type: 'application/image' });
+                                const blob = new Blob([imageBlob], { type: 'application/image' });
                 const unsafeImg = URL.createObjectURL(blob);
                 const image: SafeUrl = this.sanitizer.bypassSecurityTrustUrl(unsafeImg) ;
                 return image;
