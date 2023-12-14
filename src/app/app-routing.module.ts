b773da160d5ca51ac9ea064e2d039f83e85c3ddf
allana <mohamad.allan@rigd-loxia.nl>
2023-12-14 09:29:31 +0100

    authencication protec route not working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { HomescreenComponent } from './components/homescreen/homescreen.componen
 import { brillenLijstComponent } from './components/brillen-lijst/brillen-lijst.component';
 import { BrilAdvertentieComponent } from './components/bril-advertentie/bril-advertentie.component';
 import { SubscriptionComponent } from './components/subscription/subscription.component';
+import {AuthGuard} from "@auth0/auth0-angular";
 
 const routes: Routes = [
   { path: '', component: AppComponent },
-  { path: 'brilgevonden', component: BrilgevondenComponent },
+  { path: 'brilgevonden', component: BrilgevondenComponent, canActivate: [AuthGuard] },
   { path: 'homescreen', component: HomescreenComponent },
-  { path: 'gevondenbrillen', component: brillenLijstComponent },
+  { path: 'gevondenbrillen', component: brillenLijstComponent, canActivate: [AuthGuard] },
   { path: 'briladvertentie', component: BrilAdvertentieComponent },
   { path: 'subscriptions', component: SubscriptionComponent },
 
