b68ff6516d844c7bcd0f09a25bb0316b66535e90
allana <mohamad.allan@rigd-loxia.nl>
2024-01-17 12:42:26 +0100

    chat work in progress

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,10 +1,130 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
+import {AuthService, User} from "@auth0/auth0-angular";
+import {FormControl} from "@angular/forms";
+import {Observable, of} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {Client, IFrame, Stomp} from "@stomp/stompjs";
+import {Message} from "../../dao/message";
+import * as SockJS from "sockjs-client";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked{
+  url = 'http://localhost:8080';
+  otherUser?: User;
+  thisUser: User;
+  channelName?: string;
+  socket?: WebSocket;
+  stompClient?: Client;
+  newMessage = new FormControl('');
+  messages?: Observable<Array<Message>>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http:HttpClient,
+    private el: ElementRef,
+    public auth: AuthService) {}
+
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(user => {
+      debugger;
+      this.thisUser = user!;
+      this.connectToChat();
+      this.el.nativeElement.querySelector("#chat").scrollIntoView();
+
+    });
+
+  }
+  ngOnDestroy(): void {
+    // Close the WebSocket connection when the component is destroyed
+    if (this.stompClient) {
+      this.stompClient.deactivate();
+    }
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollDown();
+  }
+
+  scrollDown(){
+    var container = this.el.nativeElement.querySelector("#chat");
+    container.scrollTop = container.scrollHeight;
+  }
+
+  connectToChat() {
+    const id1 = this.thisUser.email!;
+    const nick1 = this.thisUser.nickname;
+    const id2 = this.otherUser?.email!;
+    const nick2 = this.otherUser?.nickname!;
+
+    if (id1 > id2) {
+      this.channelName = nick1 + '&' + nick2;
+    } else {
+      this.channelName = nick2 + '&' + nick1;
+    }
+    this.loadChat();
+    console.log('connecting to chat...');
+    this.socket = new SockJS(this.url + '/chat');
+    this.stompClient = Stomp.over(this.socket);
+
+    this.stompClient.onConnect = (frame:IFrame) => {
+      //func = what to do when connection is established
+      console.log('connected to: ' + frame);
+      this.stompClient!.subscribe(
+        '/topic/messages/' + this.channelName,
+        (response) => {
+          //func = what to do when client receives data (messages)
+          this.loadChat();
+        }
+      );
+    };
+    // Handle errors
+    this.stompClient.onStompError = (frame) => {
+      console.error('Stomp error:', frame);
+    };
+
+    // Connect to the WebSocket
+    this.stompClient.activate();
+  }
+
+  sendMsg() {
+    if (this.newMessage.value !== '') {
+      // Use publish instead of send
+      this.stompClient!.publish({
+        destination: '/app/chat/' + this.channelName,
+        body: JSON.stringify({
+          sender: this.thisUser.nickname,
+          t_stamp: 'to be defined in server',
+          content: this.newMessage.value,
+        }),
+      });
+
+      this.newMessage.setValue('');
+    }
+  }
+
+  loadChat(){
+    this.messages = this.http.post<Array<Message>>(this.url+'/getMessages' ,  this.channelName);
+    this.messages.subscribe(data => {
+      let mgs:Array<Message> = data;
+      mgs.sort((a, b) => (a.ms_id > b.ms_id) ? 1 : -1)
+      this.messages = of(mgs);
+    })
+    console.log(this.messages);
+  }
+
+  whenWasItPublished(myTimeStamp: string) {
+    const endDate = myTimeStamp.indexOf('-');
+    return (
+      myTimeStamp.substring(0, endDate) +
+      ' at ' +
+      myTimeStamp.substring(endDate + 1)
+    );
+  }
 
 }
---
+++ src/app/dao/message.ts
@@ -0,0 +1,10 @@
+export class Message {
+
+  constructor(public ms_id: string,
+              public sender: string,
+              public content: string,
+              public t_stamp: string) {
+  }
+
+
+}
