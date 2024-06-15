05cd8cd439f8d78b3623dd0085080301b98c6caa
allana <mohamad.allan@rigd-loxia.nl>
2024-06-15 11:08:03 +0200

    setup firebase

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,37 +1,37 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Bril } from '../dao/bril';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Bril } from '../dao/bril';
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 export class ApiService {
 
-  baseURL: string = "http://localhost:8083/brilkwijt/";
+    baseURL: string = 'https://brilkwijtbackend-e47bf8e71893.herokuapp.com/brilkwijt/';
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-getImages(httpParams: HttpParams): Observable<Blob> {
-    return this.http.get(this.baseURL + 'brilImage', { params: httpParams, responseType: 'blob' });
-  }
+    getImages(httpParams: HttpParams): Observable<Blob> {
+        return this.http.get(this.baseURL + 'brilImage', { params: httpParams, responseType: 'blob' });
+    }
 
-  getAllBrillen(): Observable<any[]> {
-    return this.http.get<Bril[]>(this.baseURL + 'brillen')
-  }
+    getAllBrillen(): Observable<any[]> {
+        return this.http.get<Bril[]>(this.baseURL + 'brillen');
+    }
 
-  getBril(id : string): Observable<Bril> {
-    return this.http.get<Bril>(this.baseURL + 'bril/' + id)
-  }
+    getBril(id: string): Observable<Bril> {
+        return this.http.get<Bril>(this.baseURL + 'bril/' + id);
+    }
 
-  addFakeBril(fakebril:FormData): Observable<any> {
+    addFakeBril(fakebril: FormData): Observable<any> {
 
-    console.log(fakebril)
-    return this.http.post<FormData>(this.baseURL + 'description', fakebril);
-  }
+        console.log(fakebril);
+        return this.http.post<FormData>(this.baseURL + 'description', fakebril);
+    }
 
-  getAllChats(): Observable<any> {
-    return this.http.get(this.baseURL + 'getChats');
+    getAllChats(): Observable<any> {
+        return this.http.get(this.baseURL + 'getChats');
 
-  }
+    }
 
 }
