f8bbf74b6006fab10bd16994ac53be5deacacf34
allana <mohamad.allan@rigd-loxia.nl>
2024-06-15 12:50:52 +0200

    fixing login

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import 'zone.js/plugins/zone-error'; // Included with Angular CLI.
+
+export const environment = {
+  production: false,
+  storeDevTools: true,
+    redirect_uri: 'http://localhost:4200/homescreen',
+};
+
